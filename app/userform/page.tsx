33add9d5ce8fd452bf020bc46bde6e4d979ed0af
unknown <melyssa1857@gmail.com>
2026-05-12 10:45:33 +0700

    navbar intitial

---
+++ app/userform/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav>
+          <h5 className="text-[#1A1A2E] font-bold">KaiFong AI</h5>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar
